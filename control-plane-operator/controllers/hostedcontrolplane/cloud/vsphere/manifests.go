caa40120db2f6e1695cb1b76c1370bc70a365978
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2023-06-14 13:32:42 -0400

    deploy vsphere cloud controller manager

---
+++ control-plane-operator/controllers/hostedcontrolplane/cloud/vsphere/manifests.go
@@ -0,0 +1,58 @@
+package vsphere
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func CCMServiceAccount(ns string) *corev1.ServiceAccount {
+	return &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "vsphere-cloud-controller-manager",
+			Namespace: ns,
+		},
+	}
+}
+
+func CCMDeployment(ns string) *appsv1.Deployment {
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "vsphere-cloud-controller-manager",
+			Namespace: ns,
+		},
+	}
+}
+
+func ccmContainer() *corev1.Container {
+	return &corev1.Container{
+		Name: "cloud-controller-manager",
+	}
+}
+
+func ccmVolumeKubeconfig() *corev1.Volume {
+	return &corev1.Volume{
+		Name: "kubeconfig",
+	}
+}
+
+func ccmCloudConfig() *corev1.Volume {
+	return &corev1.Volume{
+		Name: "cloud-config",
+	}
+}
+
+func ccmCloudControllerCreds() *corev1.Volume {
+	return &corev1.Volume{
+		Name: "cloud-controller-creds",
+	}
+}
+
+func KubeCloudControllerCredsSecret(controlPlaneNamespace string) *corev1.Secret {
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: controlPlaneNamespace,
+			Name:      "cloud-controller-creds",
+		},
+	}
+}
